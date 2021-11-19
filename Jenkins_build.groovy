17a2005c3c786de9f88c06a6bc3109815259ca1c
gururepala <guru.repala@gmail.com>
2021-11-19 22:59:14 +0530

    Commited

--- Jenkins_build.groovy
+++ Jenkins_build.groovy
@@ -1,11 +1,15 @@
 //Descriptive pipeline
 pipeline{
     agent any
+    environment {
+        BRANCH = "${env.BRANCH_NAME}"
+    }
     stages{
         stage("Cloning the code from GITHUB URL") {
             steps {
                 println "Using the URL copied from GIT HUB repository we are cloning the code"
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[ url: 'https://github.com/gururepala/boxfuse-sample-java-war-hello.git']]])
+                //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[ url: 'https://github.com/gururepala/boxfuse-sample-java-war-hello.git']]])
+                git branch: "${BRANCH_NAME}"
                 sh """
                 ls -al
                 """
@@ -24,7 +28,7 @@ pipeline{
         stage("Builded code will be copied to artifacts") {
             steps {
                 println "Builded code will be uploaded to S3 bucket"
-                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mydeployedprojects/${BUILD_NUMBER}/"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mydeployedprojects/${BRANCH_NAME}/${BUILD_NUMBER}/"
             }   
         }
 
