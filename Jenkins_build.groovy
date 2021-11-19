a9e7fc79d24401fe6a4665a430a5b9ad2b5f6f4c
gururepala <guru.repala@gmail.com>
2021-11-19 19:30:24 +0530

    First Commit

---
+++ Jenkins_build.groovy
@@ -0,0 +1,34 @@
+//Descriptive pipeline
+pipeline{
+    agent any
+    stages{
+        stage("Cloning the code from GITHUB URL") {
+            steps {
+                println "Using the URL copied from GIT HUB repository we are cloning the code"
+                sh """
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/gururepala/boxfuse-sample-java-war-hello.git']]])
+                ls -al
+                """
+            }        
+        }
+        stage("Building the code") {
+            steps {
+                println "Copied GIT repository code will be builded using maven command"
+                sh """
+                mvn clean package
+                echo "$BUILD_NUMBER"
+                ls -al /target/
+                """
+            }
+        }
+        stage("Builded code will be copied to artifacts") {
+            steps {
+                println "Builded code will be uploaded to S3 bucket"
+                sh """
+                aws s3 /target/hello-$BUILD_NUMBER.war s3://mydeployedprojects/
+                """
+            }
+        }
+
+    }
+}
