d1188c5168d2ebe5578d86094f2ccc00203a4768
gururepala <guru.repala@gmail.com>
2021-11-19 23:43:39 +0530

    1st commit

--- Jenkins_deploy.groovy
+++ Jenkins_deploy.groovy
@@ -1,10 +1,16 @@
 //Declarative pipeline
 pipeline{
     agent any 
+    parameters {
+    string(name: 'BUILD_NUMBER', defaultValue: '', description: 'From which buildnumber artifacts are doenloaded?')
+  }
     stages{
         stage("Download file from S3 Bucket") {
             steps {
                     println "Downloading build code from S3"
+                    sh "aws s3 ls"
+                    sh "aws s3 ls s3://mydeployedprojects"
+                    sh "aws s3 ls s3://mydeployedprojects/${BUILD_NUMBER}/"
             }
         }
         stage("copy file to tomcat") {
