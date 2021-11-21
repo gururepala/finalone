4178d8f283e4542d75745ec66639724f1ee25e49
gururepala <guru.repala@gmail.com>
2021-11-21 15:48:09 +0530

    2nd commit

--- Jenkins_deploy.groovy
+++ Jenkins_deploy.groovy
@@ -4,6 +4,7 @@ pipeline{
     parameters {
     string(name: 'BRANCH_NAME', defaultValue: '${BARNCH}    ', description: 'From which branch artifacts are downloaded?')
     string(name: 'BUILD_NUMBER', defaultValue: '', description: 'From which buildnumber artifacts are downloaded?')
+    string(name: 'SERVERIP', defaultValue: '', description: 'To which server builded code shall be deployed?')
   }
     stages{
         stage("Download file from S3 Bucket") {
@@ -12,11 +13,13 @@ pipeline{
                     sh "aws s3 ls"
                     sh "aws s3 ls s3://mydeployedprojects"
                     sh "aws s3 ls s3://mydeployedprojects/${BRANCH_NAME}/${BUILD_NUMBER}/"
+                    sh "aws s3 cp s3://mydeployedprojects/${BRANCH_NAME}/${BUILD_NUMBER}/hello-${BUILD_NUMBER}.war ."
             }
         }
         stage("copy file to tomcat") {
             steps {
                 println "Deploying build code to tomcat server"
+                sh "ssh -i /tmp/DevOpsNV.pem ec2-user@${SERVERIP} "systemctl status tomcat""
             }
         }
     }
