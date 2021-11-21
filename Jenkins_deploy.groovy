12c863fbe657cf12a9f1edd2f6f456e8a4a17a63
gururepala <guru.repala@gmail.com>
2021-11-21 17:22:27 +0530

    committed again

--- Jenkins_deploy.groovy
+++ Jenkins_deploy.groovy
@@ -19,7 +19,7 @@ pipeline{
         stage("copy file to tomcat server") {
             steps {
                 println "Deploying build code to tomcat server"
-                //sh "ssh -i /tmp/DevOpsNV.pem ec2-user@${SERVERIP} \"systemctl status tomcat\""
+                sh "ssh -i /tmp/DevOpsNV.pem ec2-user@${SERVERIP} \"systemctl status tomcat\""
                 sh "scp -i /tmp/DevOpsNV.pem hello-${BUILD_NUMBER}.war ec2-user@${SERVERIP}:/var/lib/tomcat/webapps"
             }
         }
