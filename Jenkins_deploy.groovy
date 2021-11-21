52c7763b2d0b71c137b3550828c0046c52dc86fd
gururepala <guru.repala@gmail.com>
2021-11-21 15:35:28 +0530

    commited commit

--- Jenkins_deploy.groovy
+++ Jenkins_deploy.groovy
@@ -2,7 +2,8 @@
 pipeline{
     agent any 
     parameters {
-    string(name: 'BUILD_NUMBER', defaultValue: '', description: 'From which buildnumber artifacts are doenloaded?')
+    string(name: 'BRANCH_NAME', defaultValue: '${BARNCH}    ', description: 'From which branch artifacts are downloaded?')
+    string(name: 'BUILD_NUMBER', defaultValue: '', description: 'From which buildnumber artifacts are downloaded?')
   }
     stages{
         stage("Download file from S3 Bucket") {
@@ -10,7 +11,7 @@ pipeline{
                     println "Downloading build code from S3"
                     sh "aws s3 ls"
                     sh "aws s3 ls s3://mydeployedprojects"
-                    sh "aws s3 ls s3://mydeployedprojects/${BUILD_NUMBER}/"
+                    sh "aws s3 ls s3://mydeployedprojects/${BRANCH_NAME}/${BUILD_NUMBER}/"
             }
         }
         stage("copy file to tomcat") {
